665a4cace19be0740a555549a8a3fbe799fef261
Abdelrahman Ahmed <abdelm.is@gmail.com>
2016-06-29 14:16:24 +1000

    Added nuke file - getting tracks from playlist

---
+++ nuke.js
@@ -0,0 +1,33 @@
+var SpotifyWebApi = require('spotify-web-api-node');
+
+if (!process.env.PRODUCTION) {
+  require('dotenv').load();
+}
+
+var today      = new Date();
+var spotifyApi = new SpotifyWebApi({
+  clientId     : process.env.SPOTIFY_KEY,
+  clientSecret : process.env.SPOTIFY_SECRET,
+  redirectUri  : process.env.SPOTIFY_REDIRECT_URI
+});
+
+// Execute on Mondays
+//if (today.getDay() == 1) {
+  spotifyApi.getPlaylistTracks(process.env.SPOTIFY_USERNAME, process.env.SPOTIFY_PLAYLIST_ID)
+    .then(function(data) {
+      var tracks = data.body.items;
+      var deleteTracks = [];
+
+      for (var i = 0; i < 100; i++) {
+        deleteTracks.push({uri: tracks[i].uri});
+      }
+
+      console.log(deleteTracks);
+      
+      /*
+      spotifyApi.removeTracksFromPlaylist(process.env.SPOTIFY_USERNAME, process.env.SPOTIFY_PLAYLIST_ID)
+        .then(function(data) {
+
+      });*/
+    });
+//}
